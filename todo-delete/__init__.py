d91f4bf4943c0e629fe97badb04ea9daa1422b22
John Lusk <luskbo82@gmail.com>
2023-07-27 14:13:40 -0500

    updating

--- todo-delete/__init__.py
+++ todo-delete/__init__.py
@@ -5,7 +5,7 @@ from services.todo_service import delete_todos
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function processed a requjjjest.')
 
     try:
         task_id = req.route_params.get('id')
